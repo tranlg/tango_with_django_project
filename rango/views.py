c8b0735c5a37f2085b706e762539583e54f0e305
Linda Tran <2472123T@student.gla.ac.uk>
2021-01-22 16:46:48 +0000

    Chapter 4 Exercises

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Linda'}
+    return render(request, 'rango/about.html', context=context_dict)
