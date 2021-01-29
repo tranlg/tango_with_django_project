2f53723055bc919b4cf45f0be2f5b755a4826ef7
Linda Tran <2472123T@student.gla.ac.uk>
2021-01-29 16:50:26 +0000

    Chapter 7

---
+++ rango/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from rango.models import Category, Page
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional info on the form.
+    class Meta:
+        # Provide association between the ModelForm and a model
+        model = Category
+        fields = ('name', )
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.CharField(help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://'
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url']=url
+        return cleaned_data
+
+    class Meta:
+        model = Page
+        exclude = ('category', )
+        # not to include foreign key or specify the fields to inclute fields = ('title', 'url', 'views', )
